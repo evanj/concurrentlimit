b9ce98ee3a6a31a21b184cda9bb987934d9e104c
Evan Jones <ej@evanjones.ca>
2020-03-01 15:04:10 -0500

    add client for testing memory limits
    sleepyserver: Add flags to limit concurrency. It works!

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/golang/protobuf v1.3.4
+	golang.org/x/net v0.0.0-20190311183353-d8887717615a
 	google.golang.org/grpc v1.27.1
 )
--- sleepyserver/sleepyserver.go
+++ sleepyserver/sleepyserver.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -14,14 +15,18 @@ import (
 
 	"github.com/evanj/concurrentlimit/sleepymemory"
 	"github.com/golang/protobuf/ptypes"
+	"golang.org/x/net/netutil"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 const sleepHTTPKey = "sleep"
 const wasteHTTPKey = "waste"
 
 type server struct {
-	requests concurrentMaxLogger
+	logger  concurrentMaxLogger
+	limiter limiter
 }
 
 func (s *server) rawRootHandler(w http.ResponseWriter, r *http.Request) {
@@ -36,7 +41,11 @@ func (s *server) rawRootHandler(w http.ResponseWriter, r *http.Request) {
 
 	err := s.rootHandler(w, r)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		statusCode := http.StatusInternalServerError
+		if err == errLimited {
+			statusCode = http.StatusTooManyRequests
+		}
+		http.Error(w, err.Error(), statusCode)
 	}
 }
 
@@ -85,19 +94,34 @@ func (s *server) rootHandler(w http.ResponseWriter, r *http.Request) error {
 		req.WasteBytes = int64(bytes)
 	}
 
-	_, err := s.Sleep(r.Context(), req)
+	resp, err := s.sleepImplementation(r.Context(), req)
 	if err != nil {
 		return err
 	}
 
 	w.Header().Set("Content-Type", "text/plain;charset=utf-8")
-	fmt.Fprintf(w, "slept for %s (pass ?sleep=x)\nwasted %d bytes (pass ?waste=y)\n",
-		req.SleepDuration.String(), req.WasteBytes)
+	fmt.Fprintf(w, "slept for %s (pass ?sleep=x)\nwasted %d bytes (pass ?waste=y)\nignored response=%d\n",
+		req.SleepDuration.String(), req.WasteBytes, resp.Ignored)
 	return nil
 }
 
 func (s *server) Sleep(ctx context.Context, request *sleepymemory.SleepRequest) (*sleepymemory.SleepResponse, error) {
-	defer s.requests.start()()
+	resp, err := s.sleepImplementation(ctx, request)
+	if err == errLimited {
+		err = status.Error(codes.ResourceExhausted, err.Error())
+	}
+	return resp, err
+}
+
+func (s *server) sleepImplementation(ctx context.Context, request *sleepymemory.SleepRequest) (*sleepymemory.SleepResponse, error) {
+	// limit concurrent requests
+	end, err := s.limiter.start()
+	if err != nil {
+		return nil, err
+	}
+	defer end()
+
+	defer s.logger.start()()
 
 	wasteSlice := make([]byte, request.WasteBytes)
 	// touch each page in the slice to ensure it is actually allocated
@@ -114,15 +138,22 @@ func (s *server) Sleep(ctx context.Context, request *sleepymemory.SleepRequest) 
 			return nil, err
 		}
 	}
+	// TODO: use ctx for cancellation
 	time.Sleep(duration)
 
-	return &sleepymemory.SleepResponse{}, nil
+	// read some of the memory and return it so it doesn't get garbage collected
+	total := 0
+	for i := 0; i < len(wasteSlice); i += 10 * pageSize {
+		total += int(wasteSlice[i])
+	}
+
+	return &sleepymemory.SleepResponse{Ignored: int64(total)}, nil
 }
 
 type concurrentMaxLogger struct {
 	mu      sync.Mutex
-	current int
 	max     int
+	current int
 }
 
 // start records the start of a concurrent request that is terminated when func is called.
@@ -147,17 +178,76 @@ func (c *concurrentMaxLogger) end() {
 	c.mu.Unlock()
 }
 
+var errLimited = errors.New("Limiter.start: exceeded limit of concurrent requests")
+
+type concurrentLimiter struct {
+	mu      sync.Mutex
+	max     int
+	current int
+}
+
+// start records the start of a concurrent request that is terminated when func is called.
+func (c *concurrentLimiter) start() (func(), error) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	next := c.current + 1
+	if next > c.max {
+		return nil, errLimited
+	}
+	c.current = next
+
+	return c.end, nil
+}
+
+func (c *concurrentLimiter) end() {
+	c.mu.Lock()
+	c.current--
+	if c.current < 0 {
+		panic("bug: mismatched calls to start/end")
+	}
+	c.mu.Unlock()
+}
+
+type limiter interface {
+	start() (func(), error)
+}
+
+type nilLimiter struct{}
+
+func doNothing() {}
+
+func (n *nilLimiter) start() (func(), error) {
+	return doNothing, nil
+}
+
 func main() {
 	httpAddr := flag.String("httpAddr", "localhost:8080", "Address to listen for HTTP requests")
 	grpcAddr := flag.String("grpcAddr", "localhost:8081", "Address to listen for gRPC requests")
+	concurrentRequests := flag.Int("concurrentRequests", 0, "Limits the number of concurrent requests")
+	concurrentConnections := flag.Int("concurrentConnections", 0, "Limits the number of concurrent connections")
 	flag.Parse()
 
-	s := &server{}
+	s := &server{concurrentMaxLogger{}, &nilLimiter{}}
+	if *concurrentRequests > 0 {
+		log.Printf("limiting the server to %d concurrent requests", *concurrentRequests)
+		s.limiter = &concurrentLimiter{sync.Mutex{}, *concurrentRequests, 0}
+	}
+
 	http.HandleFunc("/", s.rawRootHandler)
 	http.HandleFunc("/stats", s.memstatsHandler)
-	log.Printf("listening on http://%s ...", *httpAddr)
+	log.Printf("listening for HTTP on http://%s ...", *httpAddr)
+	httpListener, err := net.Listen("tcp", *httpAddr)
+	if err != nil {
+		panic(err)
+	}
+	if *concurrentConnections > 0 {
+		log.Printf("limiting the HTTP server to %d concurrent connections", *concurrentConnections)
+		httpListener = netutil.LimitListener(httpListener, *concurrentConnections)
+	}
+
 	go func() {
-		err := http.ListenAndServe(*httpAddr, nil)
+		err := http.Serve(httpListener, nil)
 		if err != nil {
 			panic(err)
 		}
@@ -168,6 +258,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	if *concurrentConnections > 0 {
+		log.Printf("limiting the gRPC server to %d concurrent connections", *concurrentConnections)
+		grpcListener = netutil.LimitListener(grpcListener, *concurrentConnections)
+	}
+
 	grpcServer := grpc.NewServer()
 	sleepymemory.RegisterSleeperServer(grpcServer, s)
 	err = grpcServer.Serve(grpcListener)
